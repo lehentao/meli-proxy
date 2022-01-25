d833c85f013a4b8fea152ac5175789086958b5a6
gsrichiardi <gsrichiardi@falabella.cl>
2022-01-25 14:04:54 -0300

    change port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const displayRoutes = require('express-routemap');
 const fs = require('fs');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
 
 const port = process.env.SERVER_PORT || 5000;
 app.use('/', require('./routes'));
@@ -24,3 +26,38 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
   displayRoutes(app);
 });
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
+
+exports.closeServer = function () {
+  server.close();
+};
